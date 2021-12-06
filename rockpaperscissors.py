2fb01d690d1e4eaaf8fbf6c5aecbe9fc7980090a
Samson <sbadlia2002@gmail.com>
2021-12-05 22:48:54 -0500

    Fixed and tested winning cases

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -6,6 +6,15 @@ computer = random.choice(choices)
 
 print(f"\n You chose {user}, computer chose {computer}.\n")
 
+def printWinPhrase():
+    print(f"{user} beats {computer} which means you win")
+
+def printLossPhrase():
+    print(f"{computer} beats {user} which means you dead lost to the computer bro")
+
+def printTiePhrase():
+    print(f"Both yall selected {user}. It's a tie!")
+
 if user == computer: printTiePhrase()
 elif user == "rock":
     if computer == "scissors": printWinPhrase()
@@ -16,12 +25,3 @@ elif user == "paper":
 elif user == "scissors":
     if computer == "paper": printWinPhrase()
     else: printLossPhrase()
-
-def printWinPhrase():
-    print(f"{user} beats {computer} which means you win")
-
-def printLossPhrase():
-    print(f"{computer} beats {user} which means you dead lost to the computer bro")
-
-def printTiePhrase():
-    print(f"Both yall selected {user}. It's a tie!")
